a6f485958ff736f6d6d1757d50639ae0a23fc69b
Beatriz L <beatrizlima.tds1.senai@gmail.com>
2025-03-10 23:00:17 -0300

    commit

---
+++ secondmobile/pages/Details.js
@@ -0,0 +1,7 @@
+export default function Details() {
+    <View style={styles.container}>
+        <Text style={styles.title}>Detalhes</Text>
+        <Text style={styles.vermText}>Sem persistência: nenhum texto salvo</Text>
+        <Text style={styles.verdText}>Texto persistido: nenhum texto salvo</Text>
+    </View>
+}
--- secondmobile/pages/Home.js
+++ secondmobile/pages/Home.js
@@ -1,9 +1,13 @@
-import { TextInput } from "react-native";
-
-export default function Home() {
+export default function Home(){
     return (
-        <View>
-            <Text>daoooora</Text>
+        <View style={styles.container}>
+            <Text style={styles.title}>Persistência e Navegação</Text>
+            <TextInput style={styles.input}>Digite algo</TextInput>
+            <Text style={styles.vermText}>Sem persistência: nenhum texto salvo</Text>
+            <Text style={styles.verdText}>Texto persistido: nenhum texto salvo</Text>
+            <Button style={styles.button}>Salvar</Button>
+            <Button style={styles.button}>Limpar</Button>
+            <Button style={styles.button}>Detalhes</Button>
         </View>
     )
 }
