f0ceb2e6dff69220b6a3daf5221705d67cd491f4
Beatriz L <beatrizlima.tds1.senai@gmail.com>
2025-04-08 16:10:08 -0300

    refactor: app.js code

--- secondmobile/App.js
+++ secondmobile/App.js
@@ -1,13 +1,17 @@
 import React from "react";
-import { View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
 import HomeScreen from "./pages/HomeScreen";
 import DetailsScreen from "./pages/DetailsScreen";
 
 export default function App() {
     return (
-        <View>
-        <HomeScreen />
-        <DetailsScreen />
-        </View>
+        <NavigationContainer>
+            <Stack.Navigator initialRouteName="Home">
+                <Stack.Screen name="Home" component={HomeScreen} />
+                <Stack.Screen name="Details" component={DetailsScreen} />
+            </Stack.Navigator>
+        </NavigationContainer>
     );
 }
