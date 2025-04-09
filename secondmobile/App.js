21c0916e3b27fe193fb102d39a2022e5fdafeac1
Beatriz L <beatrizlima.tds1.senai@gmail.com>
2025-04-08 23:17:20 -0300

    FINISHED :)

--- secondmobile/App.js
+++ secondmobile/App.js
@@ -2,15 +2,17 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 
-import HomeScreen from "./pages/HomeScreen";
-import DetailsScreen from "./pages/DetailsScreen";
+import HomeScreen from "./../secondmobile/pages/HomeScreen";
+import DetalhesScreen from "./../secondmobile/pages/DetailsScreen";
+
+const Stack = createStackNavigator();
 
 export default function App() {
     return (
         <NavigationContainer>
             <Stack.Navigator initialRouteName="Home">
                 <Stack.Screen name="Home" component={HomeScreen} />
-                <Stack.Screen name="Details" component={DetailsScreen} />
+                <Stack.Screen name="Detalhes" component={DetalhesScreen} />
             </Stack.Navigator>
         </NavigationContainer>
     );
